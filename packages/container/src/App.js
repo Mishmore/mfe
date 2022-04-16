41c017c38ecb7b4d67b194a83faf698d2380330f
Mishmore <michellemore28@gmail.com>
2022-04-16 12:38:55 -0500

    change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hellou</h1>
+      <h1>Hello</h1>
       <MarketingApp />
     </div>
   );
